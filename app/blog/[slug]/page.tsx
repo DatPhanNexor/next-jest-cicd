0985dce9bfce93d786b50f0512a68b5ab6d33b98
Python Apex <pythonapex01@gmail.com>
2026-04-05 23:12:34 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -20,7 +20,7 @@ export default function Page({ params }: Params) {
   return (
     <>
       <h1>Slug: {params.slug}</h1>
-      <p>Hole Tex</p>
+      <p>HoleTex</p>
       <p>Welcome to HCM</p>
     </>
   );
